541b2749c176c95f670579d63d325193d76682e8
Ujjawal <11.ujjawal@gmail.com>
2016-11-28 16:50:48 +0530

    tree max elem

---
+++ btree/max-elem.cpp
@@ -0,0 +1,48 @@
+/* Find the maximum element in a binary tree */
+
+#include <iostream>
+#include <limits>
+#include "tree.h"
+
+using namespace std;
+
+/* Template declaration for finding the maximum value of an element of the given type */
+template <typename T>
+T getMax(btNode<T> const& node);
+
+int main(void) {
+    auto const root = make_node<double>(-5.00001);
+    root->left = make_node<double>(-9.5);
+    root->right = make_node<double>(-3.23);
+    root->left->left = make_node<double>(-7.45);
+    root->left->right = make_node<double>(-5.978);
+    root->right->left = make_node<double>(-2.43);
+    root->right->right = make_node<double>(-7.34);
+
+    cout << getMax(*root) << endl;
+
+    return 0;
+}
+
+/* Template definition for finding the maximum value of an element of the given type */
+template <typename T>
+T getMax(btNode<T> const& node) {
+    T leftMax, rightMax, max = node.data;
+
+    /* leftMax and rightMax are initialized to the lowest possible values */
+    leftMax = rightMax = numeric_limits<T>::lowest();
+
+    if(node.left)
+        leftMax = getMax(*node.left);
+
+    if(node.right)
+        rightMax = getMax(*node.right);
+
+    if(leftMax > max)
+        max = leftMax;
+
+    if(rightMax > max)
+        max = rightMax;
+
+    return max;
+}
