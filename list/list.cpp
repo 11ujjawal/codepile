0d1c31854fbae2e3946447a9f7247bcbc8d6e395
Ujjawal <11.ujjawal@gmail.com>
2016-12-17 17:50:14 +0530

    list reverse added

--- list/list.cpp
+++ list/list.cpp
@@ -17,6 +17,23 @@ int main(void) {
     push_back(head, 30);
     print(head);
 
+    /* Insert a node after the first node */
+    push_at(head, 100);
+    print(head);
+
+    /* Fetch the length of the list */
+    cout << "\nThe list has " << length(head) << " nodes." << endl;
+
+    /* Check if a value is present in the list */
+    cout << "Is the value 30 present in the list? " << search(head, 30) << endl;
+
+    /* Check if a value is present in the list */
+    cout << "Is the value 50 present in the list? " << find(head, 50) << endl;
+
+    /* Delete the second node */
+    pop_at(head);
+    print(head);
+
     /* Delete a node from the start */
     pop_front(head);
     print(head);
@@ -24,6 +41,9 @@ int main(void) {
     /* Delete a node from the end */
     pop_back(head);
     print(head);
+
+    auto listHead = make_list({1, 2, 3, 4});
+    print(reverse(listHead));
 
     return 0;
 }
@@ -56,6 +76,17 @@ void push_back(node_ptr<T>& head, const T& key) {
     counter->next = make_node(key);
 }
 
+/* Given a reference to a node in the list, insert a new node after the given node reference */
+template <typename T>
+void push_at(node_ptr<T>& node, const T& key) {
+    if(!node)
+        return;
+
+    node_ptr<T> temp = make_node(key);
+    temp->next = node->next;
+    node->next = temp;
+}
+
 /* Given a reference to the list, delete the node from the beginning of the list */
 template <typename T>
 void pop_front(node_ptr<T>& head) {
@@ -84,6 +115,15 @@ void pop_back(node_ptr<T>& head) {
     iter->next = nullptr;
 }
 
+/* Given a reference to a node, delete the node after it */
+template <typename T>
+void pop_at(node_ptr<T>& node) {
+    if(!node && !node->next)
+        return;
+
+    node->next = node->next->next;
+}
+
 /* Given a reference to the list, print the values in the list */
 template <typename T>
 void print(const node_ptr<T>& head) {
@@ -97,4 +137,80 @@ void print(const node_ptr<T>& head) {
     }
 
     cout << "nullptr" << endl;
+}
+
+/* Given a reference to the head of a list, fetch the length */
+template <typename T>
+size_t length(const node_ptr<T>& head) {
+    size_t counter = 0;
+    node_ptr<T> iter = head;
+
+    while(iter != nullptr) {
+        counter++;
+        iter = iter->next;
+    }
+
+    return counter;
+}
+
+/* Given a reference to the head of the list and a key, check for the key */
+template <typename T>
+bool search(const node_ptr<T>& head, const T& key) {
+    node_ptr<T> iter = head;
+
+    while(iter != nullptr) {
+        if(iter->data == key)
+            return true;
+
+        iter = iter->next;
+    }
+
+    return false;
+}
+
+/* Given a reference to the head of the list and a key, check for the key */
+template <typename T>
+bool find(const node_ptr<T>& head, const T& key) {
+    if(!head)
+        return false;
+
+    if(head->data == key)
+        return true;
+
+    return find(head->next, key);
+}
+
+// /* Given reference to two nodes in the list, swap the nodes */
+// template <typename T>
+// void swap(const node_ptr<T>& head, node_ptr<T>& nodeOne, node_ptr<T>& nodeTwo) {
+//     if(!nodeOne && !nodeTwo)
+//         return;
+//
+//     node_ptr<T> iter = head;
+//
+//     if(nodeOne == head) {
+//         while(iter->next != nullptr) {
+//             if
+//         }
+//     }
+// }
+
+/* Given a pointer to the head of list, reverse the list */
+template <typename T>
+node_ptr<T> reverse(node_ptr<T> head) {
+    if(!head)
+        return nullptr;
+
+    node_ptr<T> nextNode, currNode, prevNode;
+    prevNode = nullptr;
+    currNode = head;
+
+    while(currNode) {
+        nextNode = currNode->next;
+        currNode->next = prevNode;
+        prevNode = currNode;
+        currNode = nextNode;
+    }
+
+    return prevNode;
 }
--- list/list.hpp
+++ list/list.hpp
@@ -4,12 +4,15 @@
 #include <initializer_list>
 #include <memory>
 
+/* Declaring the node data structure */
 template <typename T>
 struct node;
 
+/* Defining an alias for shared pointer to node */
 template <typename T>
 using node_ptr = std::shared_ptr<node<T> >;
 
+/* Defining the node data structure */
 template <typename T>
 struct node {
     T data;
@@ -18,9 +21,27 @@ struct node {
     node(const T& value) : data(value), next(nullptr) {}
 };
 
+/* Method to make an object of the node data structure */
 template <typename T>
 auto make_node(const T& value) {
     return std::make_shared<node<T> >(value);
+}
+
+/* Method to make an object to multiple node data structure - a list */
+template <typename T>
+auto make_list(const std::initializer_list<T>& values) {
+    typename std::initializer_list<T>::iterator iter = values.begin();
+
+    node_ptr<T> temp, head;
+    temp = head = make_node(*iter);
+    iter++;
+
+    for(; iter != values.end(); ++iter) {
+        temp->next = make_node(*iter);
+        temp = temp->next;
+    }
+
+    return head;
 }
 
 /* Given a reference to the list, insert a new node at the start */
@@ -31,6 +52,10 @@ void push_front(node_ptr<T>& head, const T& key);
 template <typename T>
 void push_back(node_ptr<T>& head, const T& key);
 
+/* Given a reference to a node in the list, insert a new node after the given node reference */
+template <typename T>
+void push_at(node_ptr<T>& node, const T& key);
+
 /* Given a reference to the list, delete the node from the beginning of the list */
 template <typename T>
 void pop_front(node_ptr<T>& head);
@@ -38,6 +63,30 @@ void pop_front(node_ptr<T>& head);
 /* Given a reference to a list, delete the node from the end */
 template <typename T>
 void pop_back(node_ptr<T>& head);
+
+/* Given a reference to a node, delete the node after it */
+template <typename T>
+void pop_at(node_ptr<T>& node);
+
+/* Given a reference to the head of a list, fetch the length */
+template <typename T>
+size_t length(const node_ptr<T>& head);
+
+/* Given a reference to the head of the list and a key, check for the key - iterative method */
+template <typename T>
+bool search(const node_ptr<T>& head, const T& key);
+
+/* Given a reference to the head of the list and a key, check for the key - recursive method */
+template <typename T>
+bool find(const node_ptr<T>& head, const T& key);
+
+// /* Given reference to two nodes in the list, swap the nodes */
+// template <typename T>
+// void swap(node_ptr<T>& nodeOne, node_ptr<T>& nodeTwo);
+
+/* Given a pointer to the head of list, reverse the list */
+template <typename T>
+node_ptr<T> reverse(node_ptr<T> head);
 
 /* Given a reference to the list, print the values in the list */
 template <typename T>
