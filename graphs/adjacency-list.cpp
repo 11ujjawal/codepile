144f3e7c74f749509e51e7987ce6ab184a044262
11ujjawal <11.ujjawal@gmail.com>
2020-03-11 23:05:59 +0530

    Add adjacency list representation

--- graphs/adjacency-list.cpp
+++ graphs/adjacency-list.cpp
@@ -1,32 +1,25 @@
 #include <iostream>
 #include <vector>
+#include <list>
 
 using namespace std;
 
 class Graph {
   private:
-    vector<vector<int> > edges;
+    vector<list<int> > edges;
 
   public:
-    Graph(size_t vertices) : edges (vertices, vector<int>(vertices)) {};
-    vector<int> getAdjacentVertex(int vertex);
+    Graph(size_t vertices) : edges (vertices) {};
+    inline list<int> getAdjacentVertex(int vertex);
     inline void addEdge(int startVertex, int endVertex);
 };
 
-vector<int> Graph::getAdjacentVertex(int vertex) {
-  vector<int> vertices;
-
-  for (vector<int>::size_type index = 0; index != edges[vertex].size(); ++index) {
-    if (edges[vertex][index]) {
-      vertices.push_back(index);
-    }
-  }
-
-  return vertices;
+inline list<int> Graph::getAdjacentVertex(int vertex) {
+  return edges[vertex];
 }
 
 inline void Graph::addEdge(int startVertex, int endVertex) {
-  edges[startVertex][endVertex] = 1;
+  edges[startVertex].push_back(endVertex);
 }
 
 int main() {
